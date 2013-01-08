b45f6c48e196096f06b59a28a12a6d14ecd13ff5
Phuc PN.T <phuc@phucpnt.com>
2013-01-08 11:42:18 +0700

    Update js/equalize.js
    Add step options
      Making a group of child element same equalize instead of all.
      With the new feature, the elements on the same row of the list would have the same height.

--- js/equalize.js
+++ js/equalize.js
@@ -25,44 +25,70 @@
  * From @larsbo : http://jsfiddle.net/4QTNP/3/
  * $('.parent').equalize({children: 'p'}); // equalize height of paragraphs within .parent
  */
-;(function($) {
+;
+(function($) {
 
-  $.fn.equalize = function(options) {
-    var $containers = this, // this is the jQuery object
+    $.fn.equalize = function(options) {
+        var $containers = this, // this is the jQuery object
         children    = false,
         reset       = false,
+        step        = false,
         equalize,
         type;
 
-    // when options are an object
-    if ($.isPlainObject(options)) {
-      equalize = options.equalize || 'height';
-      children = options.children || false;
-      reset    = options.reset || false;
-    } else { // otherwise, a string was passed in or default to height
-      equalize = options || 'height';
-    }
+        // when options are an object
+        if ($.isPlainObject(options)) {
+            equalize = options.equalize || 'height';
+            children = options.children || false;
+            reset    = options.reset || false;
+            step     = options.step  || false;
+        } else { // otherwise, a string was passed in or default to height
+            equalize = options || 'height';
+        }
 
-    if (!$.isFunction($.fn[equalize])) { return false; }
+        if (!$.isFunction($.fn[equalize])) {
+            return false;
+        }
 
-    // determine if the height or width is being equalized
-    type = (equalize.indexOf('eight') > 0) ? 'height' : 'width';
+        // determine if the height or width is being equalized
+        type = (equalize.indexOf('eight') > 0) ? 'height' : 'width';
 
-    return $containers.each(function() {
-          // when children exist, equalize the passed in child elements, otherwise equalize the children
-      var $children = (children) ? $(this).find(children) : $(this).children(),
-          max = 0; // reset for each container
+        var stepChilds = new Array();
+        return $containers.each(function() {
+            // when children exist, equalize the passed in child elements, otherwise equalize the children
+            var $children = (children) ? $(this).find(children) : $(this).children(),
+            max = 0; // reset for each container
 
-      $children.each(function() {
-        var $element = $(this),
-            value;
-        if (reset) { $element.css(type, ''); } // remove existing height/width dimension
-        value = $element[equalize]();          // call height(), outerHeight(), etc.
-        if (value > max) { max = value; }      // update max
-      });
+            $children.each(function(i, el) {
+                var $element = $(el),
+                value;
+                if (reset) {
+                    $element.css(type, '');
+                } // remove existing height/width dimension
+                value = $element[equalize]();          // call height(), outerHeight(), etc.
+                if (value > max) {
+                    max = value;
+                }      // update max
 
-      $children.css(type, max +'px'); // add CSS to children
-    });
-  };
+                if(step){
+                    console.log(step);
+                    stepChilds.push($element);
+                    if((i+1) % step == 0){
+                        $.each(stepChilds, function(i, el){el.css(type, max +'px')});
+
+                        max = 0;
+                        stepChilds = new Array();
+                    }
+                    else{
+                        stepChilds.push($element);
+                    }
+                }
+            });
+
+            if(!step){
+                $children.css(type, max +'px'); // add CSS to children
+            }
+        });
+    };
 
 }(jQuery));
